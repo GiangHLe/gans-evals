10eb3f50f388256d56dcb1a2b1d136614cc8413f
GiangHLe <lhgiang149@gmail.com>
2022-12-20 18:02:23 +0900

    append all metrics into one

--- metrics.py
+++ metrics.py
@@ -1,10 +1,10 @@
 
 import numpy as np
 import scipy.linalg
-from . import metric_utils
 import torch
 
 #----------------------------------------------------------------------------
+
 def compute_is(gen_probs, num_splits=10):
     num_gen = gen_probs.shape[0]
     scores = []
@@ -15,6 +15,8 @@ def compute_is(gen_probs, num_splits=10):
         scores.append(np.exp(kl))
     return float(np.mean(scores)), float(np.std(scores))
 
+# -------------------------------------------------------------------------------------------------------------------------
+
 def compute_fid(stats_data):
     mu_real, sigma_real, mu_gen, sigma_gen = stats_data
     m = np.square(mu_gen - mu_real).sum()
@@ -22,8 +24,9 @@ def compute_fid(stats_data):
     fid = np.real(m + np.trace(sigma_gen + sigma_real - s * 2))
     return float(fid)
 
+# -------------------------------------------------------------------------------------------------------------------------
 
-def compute_kid(stats_data,  max_real=1000000, num_gen=50000, num_subsets=100):
+def compute_kid(stats_data,  max_real=1000000, num_subsets=100, max_subset_size=1000):
     real_features, gen_features = stats_data
     
     if real_features.shape[0] > max_real:
@@ -42,14 +45,11 @@ def compute_kid(stats_data,  max_real=1000000, num_gen=50000, num_subsets=100):
     kid = t / num_subsets / m
     return float(kid)
 
-# def compute_pr()
-
-
-
+# -------------------------------------------------------------------------------------------------------------------------
  
 def compute_distances(row_features, col_features, num_gpus, rank, col_batch_size):
     assert 0 <= rank < num_gpus
-    num_cols = col_features.shape[0] # 50,000
+    num_cols = col_features.shape[0] 
     num_batches = ((num_cols - 1) // col_batch_size // num_gpus + 1) * num_gpus # 5
     col_batches = torch.nn.functional.pad(col_features, [0, 0, 0, -num_cols % num_batches]).chunk(num_batches)
     dist_batches = []
@@ -62,30 +62,17 @@ def compute_distances(row_features, col_features, num_gpus, rank, col_batch_size
             dist_batches.append(dist_broadcast.cpu() if rank == 0 else None)
     return torch.cat(dist_batches, dim=1)[:, :num_cols] if rank == 0 else None
 
-#----------------------------------------------------------------------------
-#  max_real=200000, num_gen=50000, nhood_size=3, row_batch_size=10000, col_batch_size=10000)
-def compute_pr(opts, max_real, num_gen, nhood_size, row_batch_size, col_batch_size):
-    detector_url = 'https://nvlabs-fi-cdn.nvidia.com/stylegan2-ada-pytorch/pretrained/metrics/vgg16.pt'
-    detector_kwargs = dict(return_features=True)
-
-    real_features = metric_utils.compute_feature_stats_for_dataset(
-        opts=opts, detector_url=detector_url, detector_kwargs=detector_kwargs,
-        rel_lo=0, rel_hi=0, capture_all=True, max_items=max_real).get_all_torch().to(torch.float16).to(opts.device)
-
-    gen_features = metric_utils.compute_feature_stats_for_generator(
-        opts=opts, detector_url=detector_url, detector_kwargs=detector_kwargs,
-        rel_lo=0, rel_hi=1, capture_all=True, max_items=num_gen).get_all_torch().to(torch.float16).to(opts.device)
-
+def compute_pr(real_features, gen_features, kth = 3, row_batch_size=10000, col_batch_size=10000):
     results = dict()
     for name, manifold, probes in [('precision', real_features, gen_features), ('recall', gen_features, real_features)]:
         kth = []
         for manifold_batch in manifold.split(row_batch_size):
-            dist = compute_distances(row_features=manifold_batch, col_features=manifold, num_gpus=opts.num_gpus, rank=opts.rank, col_batch_size=col_batch_size)
-            kth.append(dist.to(torch.float32).kthvalue(nhood_size + 1).values.to(torch.float16) if opts.rank == 0 else None)
-        kth = torch.cat(kth) if opts.rank == 0 else None
+            dist = compute_distances(row_features=manifold_batch, col_features=manifold, col_batch_size=col_batch_size)
+            kth.append(dist.to(torch.float32).kthvalue(kth + 1).values.to(torch.float16))
+        kth = torch.cat(kth) 
         pred = []
         for probes_batch in probes.split(row_batch_size):
-            dist = compute_distances(row_features=probes_batch, col_features=manifold, num_gpus=opts.num_gpus, rank=opts.rank, col_batch_size=col_batch_size)
-            pred.append((dist <= kth).any(dim=1) if opts.rank == 0 else None)
-        results[name] = float(torch.cat(pred).to(torch.float32).mean() if opts.rank == 0 else 'nan')
+            dist = compute_distances(row_features=probes_batch, col_features=manifold, col_batch_size=col_batch_size)
+            pred.append((dist <= kth).any(dim=1))
+        results[name] = float(torch.cat(pred).to(torch.float32).mean())
     return results['precision'], results['recall']
